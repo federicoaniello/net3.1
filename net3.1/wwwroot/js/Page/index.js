7f3f865dce8ffd75a254cdc306f3e7e939d4d459
Federico <federico.aniello@exprivia.com>
2023-02-04 11:49:31 +0100

    minor fixes

--- net3.1/wwwroot/js/Page/index.js
+++ net3.1/wwwroot/js/Page/index.js
@@ -1,18 +1,16 @@
 ﻿
-
-
-var swiper = new Swiper(".product--list", {
-    slidesPerView: 1.5,
-    centeredSlides:true,
-    spaceBetween: 10,
-    navigation: true,
-    breakpoints: {
-        768: {
-            centeredSlides: false,
-            slidesPerView: 4,
-            spaceBetween: 20
-        }
-    }
-});
+//var swiper = new Swiper(".product--list", {
+//    slidesPerView: 1.5,
+//    centeredSlides:true,
+//    spaceBetween: 10,
+//    navigation: true,
+//    breakpoints: {
+//        768: {
+//            centeredSlides: false,
+//            slidesPerView: 4,
+//            spaceBetween: 20
+//        }
+//    }
+//});
 
 
